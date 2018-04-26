d05677ba64d7786678b0726d298cd6d52f1a5c64
Michael Morris <mdam2345@gmail.com>
2018-04-26 05:35:06 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,7 +24,7 @@ def full?(board)
   end
 end
 def draw?(board)
-  won?(board) == nil&& full?(board) == true
+  won?(board) == nil && full?(board) == true
 end
 def over?(board)
 won?(board) || draw?(board)
